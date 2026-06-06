2d4501d2468f4263b67f2f9b66ef99a6630e9048
Saikrishna <potharaboinasaikrishna1@gmail.com>
2026-06-06 15:36:45 +0530

    AutoSaveInput stating code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 // import ContactForm from "./Pages/Contactform/ContactForm"
 // import AcronymGenerator from "./Pages/AceonymGenerator/AcronymGenerator"
+// import UserProfile from "./Pages/userprofile/UserProfile"
 
-import UserProfile from "./Pages/userprofile/UserProfile"
+import AutoSaveInput from "./Pages/AutoSaveFile/AutoSaveFile"
 
 function App() {
-  return <UserProfile />
+  return <AutoSaveInput />
 }
 
 export default App
---
+++ src/Pages/AutoSaveFile/AutoSaveFile.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function AutoSaveInput() {
+    const [text, setText] = useState("");
+
+    return (
+        <div>
+            <h1>Auto Save Input</h1>
+
+            {/* your code here  */}
+
+        </div>
+    );
+}
+
+export default AutoSaveInput;
